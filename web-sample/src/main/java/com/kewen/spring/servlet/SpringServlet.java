bca7bed3d73b5897587c70e095828c4454fc3bff
Kewen <LiuKewenSC@outlook.com>
2023-02-03 21:17:18 +0800

    feat: 项目搭建成功

---
+++ web-sample/src/main/java/com/kewen/spring/servlet/SpringServlet.java
@@ -0,0 +1,24 @@
+package com.kewen.spring.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SpringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("servlet service1");
+        super.service(req, resp);
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("servlet service2");
+        super.service(req, res);
+    }
+}
